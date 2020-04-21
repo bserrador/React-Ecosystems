b6e82431e408bb18434671fc8b8b132c7e26a4c4
Brittany Serrador <brittanyserrador>
2020-04-21 12:37:22 -0400

    Added ToDoList components

---
+++ src/todos/ToDoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ToDoListItem from './ToDoListItem';
+
+const ToDoList = ({ todos }) => (
+    <div className="list-wrapper">
+        {todos.map(todo => <ToDoListItem todo={todo} />)}
+    </div>
+);
+
+export default ToDoList;
